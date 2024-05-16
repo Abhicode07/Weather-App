7c796c18e2d82eb7c373e37dfacadc1c16d32e77
Abhishek Sanjay Bhavsar <91628964+Abhicode07@users.noreply.github.com>
2024-05-16 14:43:42 +0530

    Add files via upload

---
+++ InfoActivity.java
@@ -0,0 +1,21 @@
+package com.example.weatherapplication;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.weatherapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.weatherapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("cz.msebera.android:httpclient:4.5.8")
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.26")
+    implementation ("com.loopj.android:android-async-http:1.4.11")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
